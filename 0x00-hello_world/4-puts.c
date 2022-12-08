adf70d3de5779e4f8c5a4fbe2e3d7e8c9331783a
sylvain tagnabou <sylvaintagnabou@gmail.com>
2022-12-08 23:13:33 +0000

    0x00 task5 2

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,12 +2,12 @@
 
 /**
  * main - Entry point
+ *
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-
-return (0);
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,11 +2,12 @@
 
 /**
  * main - Entry point
+ *
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0);
+    printf("with proper grammar, but the outcome is a piece of art,\n");
+    return (0);
 }
