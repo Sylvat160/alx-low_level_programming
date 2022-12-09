df1b064671bd44b4b9f416c2a56b03f955f62817
sylvain tagnabou <sylvaintagnabou@gmail.com>
2022-12-09 19:39:12 +0000

    0x01 -c task3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * int main - entry point
+ * Return: 0 if success
+ */
+
+int main(void)
+{
+char c;
+for(c = 'a'; c <= 'z'; c++)
+{
+putchar(c);
+}
+for(c = 'A'; c <= 'Z'; c++)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
