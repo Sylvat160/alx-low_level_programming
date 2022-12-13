f98d9fcde95eb9a3e68f93d96e54e5a506922508
sylvain tagnabou <sylvaintagnabou@gmail.com>
2022-12-13 20:56:04 +0000

    task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * times_table - prints the 9 times table, starting with 0
- * Return: the absolute value of int 
+ * times_table - prints the 9 times table, starting with 0.
+ * Return: void
  */
 
 void times_table(void)
