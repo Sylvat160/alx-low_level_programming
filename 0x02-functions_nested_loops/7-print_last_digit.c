3060552f78b572709cdf9f690b4105309be4f120
sylvain tagnabou <sylvaintagnabou@gmail.com>
2022-12-13 20:21:54 +0000

    0x02 task7 0

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,24 +1,16 @@
 #include "main.h"
 
 /**
- * print_last_digit - prints the last digit of a number
- * @n: the number to check
- * Return: the last digit of the number
+ * main - check the code for ALX School students.
+ * Return 0 if success 
  */
-
-int print_last_digit(int n)
+int main(void)
 {
-int a;
-
-if (n < 0)
-{
-n = -n;
-a = n % 10;
-}
-if (a < 0)
-{
-a = -a;
-_putchar(a + '0');
-}
-return (a);
+int r;
+print_last_digit(98);
+print_last_digit(0);
+r = print_last_digit(-1024);
+_putchar('0' + r);
+_putchar('\n');
+return (0);
 }
---
+++ 0x02-functions_nested_loops/print_last_digit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit of a number
+ * @n: the number to check
+ * Return: the last digit of the number
+ */
+
+int print_last_digit(int n)
+{
+    int a;
+
+    if (n < 0)
+    {
+        n = -n;
+        a = n % 10;
+    }
+    if (a < 0)
+    {
+        a = -a;
+        _putchar(a + '0');
+    }
+    return (a);
+}
