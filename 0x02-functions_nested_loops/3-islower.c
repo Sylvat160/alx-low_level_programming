40f3f3176a41d7e6e929edbe1cfef88e941ceff7
sylvain tagnabou <sylvaintagnabou@gmail.com>
2022-12-13 15:18:41 +0000

    0x02 task3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase character.
+ * @c: The character to print
+ * Return: 1 if c is lowercase, 0 otherwise.
+ */
+
+int _islower(int c)
+{
+
+}
