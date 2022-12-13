a0cf87e302dedbc58ae277492a5e1f9d374fd3fd
sylvain tagnabou <sylvaintagnabou@gmail.com>
2022-12-13 20:54:50 +0000

    0x02 task9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * times_table - prints the n times table, starting with 0.
+ * times_table - prints the 9 times table, starting with 0
  * Return: the absolute value of int 
  */
 
