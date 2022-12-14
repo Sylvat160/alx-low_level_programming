b8e03f56f9d51cc7a62a213c007a0169f74650dd
sylvain tagnabou <sylvaintagnabou@gmail.com>
2022-12-14 10:40:27 +0000

    0x02 task 100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,44 +1,49 @@
 #include "main.h"
 
 /**
- * print_times_table - prints the n times table
- * @n : the number to check
- * Return : void
+ * print_times_table - prints the n times table, starting with 0
+ * @n: number of the times table
  */
 void print_times_table(int n)
 {
-int a = 0, rep, b;
-if (n < 0 || n > 15)
+int i, j, k;
+if (n >= 0 && n <= 15)
 {
-return;
+for (i = 0; i <= n; i++)
+{
+for (j = 0; j <= n; j++)
+{
+k = j * i;
+if (j == 0)
+{
+_putchar(k + '0');
 }
-while (a <= n)
+else if (k < 10 && j != 0)
 {
-for (b = 0; b <= n; b++)
-{
-rep = a * b;
-if (b == 0)
-{
-_putchar('0' + rep);
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar(' ');
+_putchar(k + '0');
 }
-else if (rep < 10)
+else if (k >= 10 && k < 100)
 {
-_putchar('  ' + '0' + rep);
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar((k / 10) + '0');
+_putchar((k % 10) + '0');
 }
-else if (rep < 100)
+else if (k >= 100)
 {
-_putchar(' ' + '0' + rep / 10 + '0' + rep % 10);
-}
-else
-{
-_putchar('0' + rep / 100 + '0' + (rep - 100) / 10 + '0' + rep % 10);
-}
-if (b < n)
-{
-_putchar(', ');
+_putchar(',');
+_putchar(' ');
+_putchar((k / 100) + '0');
+_putchar(((k / 10) % 10) + '0');
+_putchar((k % 10) + '0');
 }
 }
 _putchar('\n');
-a++;
+}
 }
 }
