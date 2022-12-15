1e9365910ebb572f07f136b3c8bbb7df9ffbed8b
sylvain tagnabou <sylvaintagnabou@gmail.com>
2022-12-15 17:29:14 +0000

    11111111111111111111111111

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,11 +1,14 @@
 #include "main.h"
+
 /**
- * main - causes an infinite loop
- * Return: 0 when the code works
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
  */
+
 int main(void)
 {
-int i = 0;
+int i;
+i = 98;
 positive_or_negative(i);
 return (0);
 }
