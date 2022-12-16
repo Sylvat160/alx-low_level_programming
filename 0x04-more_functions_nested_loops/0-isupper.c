954d0e655d47add44221c435c82e9c8d694ceb04
sylvain tagnabou <sylvaintagnabou@gmail.com>
2022-12-16 20:20:16 +0000

    0x04 task0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -8,12 +8,17 @@
 
 int _isupper(int c)
 {
-if (c >= 65 && c <= 90)
+char uppercase = 'A';
+int isupper = 0;
+for (; uppercase <= 'Z'; uppercase++)
 {
-return (1);
-}
-else
+if (c == uppercase)
 {
-return (0);
+isupper = 1;
+break;
 }
+
+}
+
+return (isupper);
 }
