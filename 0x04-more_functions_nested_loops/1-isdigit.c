060120e8162b82cb829a09a417f90bea80dd8bc7
sylvain tagnabou <sylvaintagnabou@gmail.com>
2022-12-16 20:35:23 +0000

    0x04 task1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for caracter that are digits
+ * @c: character
+ * Return: 0 or 1
+ */
+
+int _isdigit(int c)
+{
+if (c >= '0' && c <= '9')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
