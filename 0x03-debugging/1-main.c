0345db0ff8c1a23d135839e28ab24c66bc79e7ed
sylvain tagnabou <sylvaintagnabou@gmail.com>
2022-12-16 19:33:46 +0000

    largest

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -11,7 +11,7 @@ int i;
 
 printf("Infinite loop incoming :(\n");
 
-i = 98;
+i = 0;
 
 /*while (i < 10)*/
 /*{*/
--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,17 +12,24 @@ int largest_number(int a, int b, int c)
 {
 int largest;
 
-if (a > b && a > c)
+if (a > b)
+{
+if (a > c)
 {
 largest = a;
 }
-else if (b > a && b > c)
+}
+else if (b > a)
+{
+if (b > c)
 {
 largest = b;
+}
 }
 else
 {
 largest = c;
 }
+
 return (largest);
 }
