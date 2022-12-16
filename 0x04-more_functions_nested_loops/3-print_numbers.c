3a6dc8d36f34dc9dccd2e52bb2d7c7cc1faa40c3
sylvain tagnabou <sylvaintagnabou@gmail.com>
2022-12-16 20:51:47 +0000

    0x04 task4

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -8,9 +8,9 @@
 void print_numbers(void)
 {
 char c;
-for (c = '0'; c ,+ '9'; c++)
+for (c = '0'; c + '9'; c++)
 {
-_putchar(c)
+_putchar(c);
 }
--_putchar('\n');
+_putchar('\n');
 }
---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_most_number - prints
+ * Return: 0
+ */
+
+void print_most_number(void)
+{
+char c;
+if (!(c == '2' || c == '4'))
+{
+_putchar(c);
+}
+_putchar('\n');
+}
