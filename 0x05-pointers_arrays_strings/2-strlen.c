bf52fc39612b692aa9a44c670e9b1c68d50329da
sylvain tagnabou <sylvaintagnabou@gmail.com>
2022-12-19 20:30:17 +0000

    0x05 task2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: pointer to a string
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+return (strlen(*s));
+}
