67c3223daa625c6a420baf794417049d16227b95
sylvain tagnabou <sylvaintagnabou@gmail.com>
2022-12-22 19:18:35 +0000

    0x05  task5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes all lowercase letters of a string to uppercase
+ * @str : string to change
+ * Return: pointer to resulting string s
+ */
+
+char *string_toupper(char *str)
+{
+int i = 0;
+while (s[i++])
+{
+if (str[i] >= 'a' && str[i] <= 'z')
+{
+str[i] -= 32;
+}
+}
+return (str);
+}
