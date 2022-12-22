e719f69610c86b72fbe1f232c0e6a9a0c00bc893
sylvain tagnabou <sylvaintagnabou@gmail.com>
2022-12-22 19:37:32 +0000

    0x06 task6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string
+ * @str : string to capitalize
+ * Return: pointer to resulting string str
+ */
+
+char *cap_string(char *str)
+{
+int i = 0;
+while (str[++i])
+{
+while (!(str[i] >= 'a' && str[i] <= 'z'))
+{
+i++;
+}
+if (str[i -1] == ' ' || str[i -1] == '\t' || str[i -1] == '\n' || str[i -1] == ',' || str[i -1] == ';' || str[i -1] == '.' || str[i -1] == '!' || str[i -1] == '?' || str[i -1] == '"' || str[i -1] == '(' || str[i -1] == ')' || str[i -1] == '{' || str[i -1] == '}')
+{
+str[i] -= 32;
+}
+}
+return (str);
+}
