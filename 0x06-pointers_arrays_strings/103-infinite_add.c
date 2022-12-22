7e2d9e43a056a432228e4c859bb0c0968aabf903
sylvain tagnabou <sylvaintagnabou@gmail.com>
2022-12-22 21:55:25 +0000

    10333333333333

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,52 +1,96 @@
 #include "main.h"
 
+/**
+ * rev_string - reverse array
+ * @n: integer params
+ * Return: 0
+ */
 
+void rev_string(char *n)
+{
+int i = 0;
+int j = 0;
+char temp;
+while (*(n + i) != '\0')
+{
+i++;
+}
+i--;
+for (j = 0; j < i; j++, i--)
+{
+temp = *(n + j);
+*(n + j) = *(n + i);
+*(n + i) = temp;
+}
+}
 
 /**
- * add_strings - Adds the numbers stored in two strings.
- * @n1: The string containing the first number to be added.
- * @n2: The string containing the second number to be added.
- * @r: The buffer to store the result.
- * @r_index: The current index of the buffer.
- *
- * Return: If r can store the sum - a pointer to the result.
- *         If r cannot store the sum - 0.
+ * infinite_add - add 2 numbers together
+ * @n1: text representation of 1st number to add
+ * @n2: text representation of 2nd number to add
+ * @r: pointer to buffer
+ * @size_r: buffer size
+ * Return: pointer to calling function
+
  */
-char *add_strings(char *n1, char *n2, char *r, int r_index)
-{
-int num, tens = 0;
-for (; *n1 && *n2; n1--, n2--, r_index--)
-{
-num = (*n1 - '0') + (*n2 - '0');
-num += tens;
-*(r + r_index) = (num % 10) + '0';
-tens = num / 10;
-}
 
-for (; *n1; n1--, r_index--)
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-num = (*n1 - '0') + tens;
-*(r + r_index) = (num % 10) + '0';
-tens = num / 10;
-}
+int overflow = 0, i = 0, j = 0, digits = 0;
+int val1 = 0, val2 = 0, temp_tot = 0;
 
-for (; *n2; n2--, r_index--)
+while (*(n1 + i) != '\0')
 {
-num = (*n2 - '0') + tens;
-*(r + r_index) = (num % 10) + '0';
-tens = num / 10;
+i++;
 }
-
-if (tens && r_index >= 0)
+while (*(n2 + j) != '\0')
 {
-*(r + r_index) = (tens % 10) + '0';
-return (r + r_index);
+j++;
 }
-
-else if (tens && r_index < 0)
+i--;
+j--;
+if (j >= size_r || i >= size_r)
 {
 return (0);
 }
-
-return (r + r_index + 1);
+while (j >= 0 || i >= 0 || overflow == 1)
+{
+if (i < 0)
+{
+val1 = 0;
+}
+else
+{
+val1 = *(n1 + i) - '0';
+}
+if (j < 0)
+{
+val2 = 0;
+}
+else
+{
+val2 = *(n2 + j) - '0';
+}
+temp_tot = val1 + val2 + overflow;
+if (temp_tot >= 10)
+{
+overflow = 1;
+}
+else
+{
+overflow = 0;
+}
+if (digits >= (size_r - 1))
+{
+return (0);
+}
+*(r + digits) = (temp_tot % 10) + '0';
+digits++;
+j--;
+}
+if (digits == size_r)
+return (0);
+*(r + digits) = '\0';
+rev_string(r);
+return (r);
 }
