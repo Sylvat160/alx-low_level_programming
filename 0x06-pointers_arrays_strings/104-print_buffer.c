7f681b76a73ee42ce152df785126d7c011efd476
sylvain tagnabou <sylvaintagnabou@gmail.com>
2022-12-22 21:39:02 +0000

    kdmfgkvdfkmv

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -2,64 +2,51 @@
 #include <stdio.h>
 
 /**
- * print_buffer - Prints a buffer 10 bytes at a time, starting with
- *                the byte position, then showing the hex content,
- *                then displaying printable charcaters.
- * @b: The buffer to be printed.
- * @size: The number of bytes to be printed from the buffer.
+ * print_buffer - prints buffer
+ * @b: buffer
+ * @size: size
+ * Return: void
  */
+
 void print_buffer(char *b, int size)
 {
-int byte, index;
-for (byte = 0; byte < size; byte += 10)
-{
-printf("%08x: ", byte);
-for (index = 0; index < 10; index++)
-{
-if ((index + byte) >= size)
-{
-printf("  ");
-}
-
-else
-{
-printf("%02x", *(b + index + byte));
-}
-
-if ((index % 2) != 0 && index != 0)
-{
-printf(" ");
-}
-
-for (index = 0; index < 10; index++)
-{
-if ((index + byte) >= size)
-{
-break;
-}
-
-else if (*(b + index + byte) >= 31 && *(b + index + byte) <= 126)
-{
-printf("%c", *(b + index + byte));
-}
-
-else
-{
-printf(".");
-}
-}
-
-if (byte >= size)
-{
-continue;
-}
-
-printf("\n");
-}
-
+int o, j, i;
+o = 0;
 if (size <= 0)
 {
 printf("\n");
+return;
 }
+
+while (o < size)
+{
+j = size - o < 10 ? size - o : 10;
+printf("%08x: ", o);
+for (i = 0; i < 10; i++)
+{
+if (i < j)
+{
+printf("%02x", *(b + o + i));
+}
+else
+{
+printf("  ");
+}
+if (i % 2)
+{
+printf(" ");
+}
+}
+for (i = 0; i < j; i++)
+{
+int c = *(b + o + i);
+if (c < 32 || c > 132)
+{
+c = '.';
+}
+printf("%c", c);
+}
+printf("\n");
+o += 10;
 }
 }
