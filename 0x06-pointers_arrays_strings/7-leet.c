996c60786a90e2c8dc1f3903067c177ca68c56dd
sylvain tagnabou <sylvaintagnabou@gmail.com>
2022-12-22 19:51:31 +0000

    0x06 task7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,5 +8,18 @@
 
 char *leet(char *s)
 {
+int i = 0, j;
+char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
 
+while (s[i++])
+{
+for (j = 0; j < 8; j++)
+{
+if (s[i] == leet[j] || s[i] - 32 == leet[j])
+{
+s[i] = j + '0';
+}
+}
+}
+return (s);
 }
