8f20f8be9d7833b8523c665f38b620bf66be264d
sylvain tagnabou <sylvaintagnabou@gmail.com>
2023-01-02 18:39:03 +0000

    0x07 task03

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: string to search
+ * @accept: string to search for
+ *
+ * Return: number of bytes in the initial segment of s which consist only of
+ * bytes from accept
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+
+}
