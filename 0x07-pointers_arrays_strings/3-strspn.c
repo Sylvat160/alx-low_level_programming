4f20f3a4190d95752011c18ef57ac9eb8fe9eed3
sylvain tagnabou <sylvaintagnabou@gmail.com>
2023-01-02 18:41:36 +0000

    0x07 task03

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -11,5 +11,20 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-
+unsigned int i, j;
+for (j = 0; *(s + j); j++)
+{
+for (i = 0; *(accept + i); i++)
+{
+if (*(s + j) == *(accept + i))
+{
+break;
+}
+}
+if (*(accept + i) == '\0')
+{
+break;
+}
+}
+return (j);
 }
