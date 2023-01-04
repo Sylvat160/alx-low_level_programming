558bf05229e36f18cd841d4255f6d1896877c926
sylvain tagnabou <sylvaintagnabou@gmail.com>
2023-01-04 22:51:50 +0000

    rsb 0x08 task 08

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * wildcmp - compares two strings and returns 1 if the strings can be considered
+ * wildcmp - compares two strings and returns 1 if t
+ * he strings can be considered
  * identical, otherwise return 0
  * @s1: pointer to a string
  * @s2: pointer to a string
