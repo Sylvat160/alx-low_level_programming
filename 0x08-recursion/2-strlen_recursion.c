a94d1f59806c54fe860dd9edeacc64a5460fa5e5
sylvain tagnabou <sylvaintagnabou@gmail.com>
2023-01-04 21:47:50 +0000

    0x08 task2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: pointer to a string
+ *
+ * Return: length of a string
+ */
+
+int _strlen_recursion(char *s)
+{
+if (*s)
+{
+return (1 + _strlen_recursion(s + 1));
+}
+else
+{
+return (0);
+}
+}
