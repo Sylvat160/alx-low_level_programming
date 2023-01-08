755363ae6536e446e1c503736d968885f065c147
sylvain tagnabou <sylvaintagnabou@gmail.com>
2023-01-08 19:26:36 +0000

    alsdk

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,15 +1,7 @@
 #include "main.h"
 
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-
 int main(void)
 {
-_puts("\"At the end of the day, my
-goal was to be the best hacker\"\n\t- Kevin Mitnick");
-return (0);
+    _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+    return (0);
 }
-
