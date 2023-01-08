d21e9a7e1bb5d1fb8c767756abbdde54de0cc0af
sylvain tagnabou <sylvaintagnabou@gmail.com>
2023-01-08 19:24:26 +0000

    rsb 0x09 task00

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,8 +1,15 @@
 #include "main.h"
 
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
 int main(void)
 {
-_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+_puts("\"At the end of the day, my
+goal was to be the best hacker\"\n\t- Kevin Mitnick");
 return (0);
 }
 
