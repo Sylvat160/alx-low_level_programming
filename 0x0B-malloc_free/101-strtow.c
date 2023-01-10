bb5430499796431f693b871b206ead89957063a1
sylvain tagnabou <sylvaintagnabou@gmail.com>
2023-01-10 20:37:03 +0000

    rsb 0x0B task05

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,10 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * strtow - splits a string into words
+ * @str: string to split
+ * Return: pointer to an array of strings
+ */
+
