36973a4a6cac1c69acfe34fb47972d58692e9d56
sylvain tagnabou <sylvaintagnabou@gmail.com>
2023-01-12 18:56:57 +0000

    setting up 0x0C main.h

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+
+#endif
