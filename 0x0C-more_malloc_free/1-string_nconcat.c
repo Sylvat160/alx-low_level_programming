0c43860cac167e0140553f172a9a3ec6643348cf
sylvain tagnabou <sylvaintagnabou@gmail.com>
2023-01-12 19:04:59 +0000

    0X0C main.h

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * string_nconcat - concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ * @n: number of bytes of s2 to concatenate
+ * Return: pointer to the concatenated string
+ */
+
+void *malloc_checked(unsigned int b)
+{
+
+}
