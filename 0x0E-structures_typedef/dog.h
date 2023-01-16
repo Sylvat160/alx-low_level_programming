7ceb6dd79274aa31987387e9810c45e8991df546
sylvain tagnabou <sylvaintagnabou@gmail.com>
2023-01-16 20:54:56 +0000

    rsb 0x0E task000

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,7 +6,6 @@
  * @name: name of dog
  * @age: age of dog
  * @owner: owner of dog
- *
  * Description: a dog
  */
 
@@ -24,7 +23,5 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-char *_strcpy(char *dest, char *src);
-int _strlen(char *s);
 
 #endif
