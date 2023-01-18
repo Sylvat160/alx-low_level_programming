df1567d8acdaebcca0ffc40411b87085db8c13d6
sylvain tagnabou <sylvaintagnabou@gmail.com>
2023-01-18 20:30:29 +0000

    header file refactoring

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stddef.h>
+#include <stdlib.h>
+
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 int int_index(int *array, int size, int (*cmp)(int));
