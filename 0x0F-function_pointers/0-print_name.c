612fe92f41c672a35ad23a9576178abf53798d47
sylvain tagnabou <sylvaintagnabou@gmail.com>
2023-01-18 20:38:19 +0000

    0x0F task00

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,2 +1,19 @@
 #include "main.h"
 #include <stdio.h>
+
+/**
+ * print_name - prints a name
+ * @name: name to print
+ * @f: function pointer
+ *
+ * Return: void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+if (name == NULL || f == NULL)
+{
+return;
+}
+f(name);
+}
