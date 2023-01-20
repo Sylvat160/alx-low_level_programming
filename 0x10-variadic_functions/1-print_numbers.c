97429dfdff891b7f0870f9c1e93322607b0ec608
sylvain tagnabou <sylvaintagnabou@gmail.com>
2023-01-20 14:38:43 +0000

    rsb 0x10 task01

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -9,17 +9,18 @@
 
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-va_list args;
-unsigned int i;
-va_start(args, n);
-for (i = 0; i < n; i++)
+va_list nums;
+unsigned int index;
+
+va_start(nums, n);
+for (index = 0; index < n; index++)
 {
-printf("%d", va_arg(args, int));
-if (i < (n - 1) && separator != NULL)
+printf("%d", va_arg(nums, int));
+if (index != n - 1 && separator != NULL)
 {
 printf("%s", separator);
 }
 }
-_putchar("\n");
-va_end(args);
+printf("\n");
+va_end(nums);
 }
