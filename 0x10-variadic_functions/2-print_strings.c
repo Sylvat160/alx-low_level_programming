de3662c8754ffed2644fb10d8fecb86496debffb
sylvain tagnabou <sylvaintagnabou@gmail.com>
2023-01-20 15:13:44 +0000

    rsb2 task02 0x10

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,4 +1,4 @@
-#include  "variadic_functions.h"
+#include "variadic_functions.h"
 
 /**
  * print_strings - prints strings, followed by a new line.
@@ -9,18 +9,15 @@
 
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-va_list ap;
+va_list vallist;
 unsigned int i;
 char *str;
 
-va_start(ap, n);
-if (n == 0)
-{
-    return (0);
-}
+va_start(vallist, n);
 for (i = 0; i < n; i++)
 {
-str = va_arg(ap, char *);
+str = va_arg(vallist, char *);
+
 if (str == NULL)
 {
 printf("(nil)");
@@ -29,11 +26,12 @@ else
 {
 printf("%s", str);
 }
-if (i != n - 1 && separator != NULL)
+
+if (i != (n - 1) && separator != NULL)
 {
 printf("%s", separator);
 }
 }
 printf("\n");
-va_end(ap);
+va_end(vallist);
 }
---
+++ 0x10-variadic_functions/main.c
@@ -0,0 +1,13 @@
+#include "variadic_functions.h"
+
+/**
+ * main - for check the code
+ * Return: 0
+ */
+
+int main(void)
+{
+    // print_numbers(", ", 4, 0, 98, -1024, 402);
+    sum_them_all(2, 98, 1024);
+    return (0);
+}
