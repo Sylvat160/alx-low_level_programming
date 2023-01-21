b239c711261f7d4019f0ae63f30e38f3e4f2a03c
sylvain tagnabou <sylvaintagnabou@gmail.com>
2023-01-21 13:29:11 +0000

    0x10 ends

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -20,25 +20,25 @@ while (format[i])
 switch (format[i])
 {
 case 'c':
-    printf("%s%c", sep, va_arg(list, int));
-    break;
+printf("%s%c", sep, va_arg(list, int));
+break;
 case 'i':
-    printf("%s%d", sep, va_arg(list, int));
-    break;
+printf("%s%d", sep, va_arg(list, int));
+break;
 case 'f':
-    printf("%s%f", sep, va_arg(list, double));
-    break;
+printf("%s%f", sep, va_arg(list, double));
+break;
 case 's':
-    str = va_arg(list, char *);
-    if (str == NULL)
-    {
-    str = "(nil)";
-    }
-    printf("%s%s", sep, str);
-    break;
+str = va_arg(list, char *);
+if (str == NULL)
+{
+str = "(nil)";
+}
+printf("%s%s", sep, str);
+break;
 default:
-    i++;
-    continue;
+i++;
+continue;
 }
 sep = ", ";
 i++;
