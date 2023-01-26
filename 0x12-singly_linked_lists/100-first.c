be0deceb9e7946386f15e9b6fc17a8ffde3cbbfa
sylvain tagnabou <sylvaintagnabou@gmail.com>
2023-01-26 23:15:27 +0000

    task100

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,13 +3,12 @@
 void __attribute__ ((constructor)) hare(void);
 
 /**
- * hare - prints a string before the main 
+ * hare - prints a string before the main
  * function is executed
- * 
  */
 
 void hare(void)
 {
-printf("You're beat! and yet, you must allow,
-\nI bore my house upon my back!\n")
+printf("You're beat! and yet, you must allow,"
+"\nI bore my house upon my back!\n");
 }
