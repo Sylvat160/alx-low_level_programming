f74a571e9cd399baa48e59f06a657bfb1ee15f2e
sylvain tagnabou <sylvaintagnabou@gmail.com>
2023-01-26 23:09:37 +0000

    0x12 task05

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * hare - prints a string before the main 
+ * function is executed
+ * 
+ */
+
+void hare(void)
+{
+printf("You're beat! and yet, you must allow,"
+"\nI bore my house upon my back!\n")
+}
