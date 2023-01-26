20306949b9da6a4e376230166168b4b2047439ad
sylvain tagnabou <sylvaintagnabou@gmail.com>
2023-01-26 23:23:46 +0000

    0x12 task101

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -9,6 +9,6 @@ void __attribute__ ((constructor)) hare(void);
 
 void hare(void)
 {
-printf("You're beat! and yet, you must allow,"
-"\nI bore my house upon my back!\n");
+printf("You're beat! and yet, you must allow,\n"
+"I bore my house upon my back!\n");
 }
