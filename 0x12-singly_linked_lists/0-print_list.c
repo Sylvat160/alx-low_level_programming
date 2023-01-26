9647608a81eadd008bbf9c5c0bb0235bcceeaba9
sylvain tagnabou <sylvaintagnabou@gmail.com>
2023-01-26 21:55:27 +0000

    0x012 list.H

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a list_t list
+ * @h: pointer to the head of the list
+ *
+ * Return: the number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+
+}
