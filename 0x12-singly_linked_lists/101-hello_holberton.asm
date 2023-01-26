c24032cb452783258b4c7f0ea8c26728e49f2d54
sylvain tagnabou <sylvaintagnabou@gmail.com>
2023-01-26 23:50:48 +0000

    alcsn

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,5 +1,5 @@
 section .data 
-    message db "Hello, Holberton", 10
+    message db "Hello, Holberton!", 10
 
 section .text 
     global _start
@@ -7,9 +7,9 @@ section .text
         mov rax, 1
         mov rdi, 1
         mov rsi, message
-        mov rdx, 17
+        mov rdx, 18
         syscall
-
+        
         mov rax, 60
         mov rdi, 0
         syscall
