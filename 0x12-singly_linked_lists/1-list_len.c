03aa3570c7d119479388af86e7fa7ea89901dd4f
sylvain tagnabou <sylvaintagnabou@gmail.com>
2023-01-26 22:46:57 +0000

    0x12 task 01

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * list_len - returns the number of elements in a linked list_t list
+ * @h: pointer to the head of the list
+ *
+ * Return: number of elements in the list
+ */
+
+size_t list_len(const list_t *h)
+{
+size_t count = 0;
+while (h)
+{
+count++;
+h = h->next;
+}
+return (count);
+}
