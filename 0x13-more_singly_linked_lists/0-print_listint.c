716215df6c4981ece32a057ad98d46cada434d44
sylvain tagnabou <sylvaintagnabou@gmail.com>
2023-01-31 20:20:10 +0000

    dgj

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,5 +1,5 @@
 #include "lists.h"
-#include "stdio.h"
+#include <stdio.h>
 
 /**
  * print_listint - prints all the elements of a listint_t list
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,6 +1,8 @@
 #ifndef LIST_H
 #define LIST_H
 
+#include <stdlib.h>
+
 /**
  * struct listint_s - singly linked list
  * @n: integer
