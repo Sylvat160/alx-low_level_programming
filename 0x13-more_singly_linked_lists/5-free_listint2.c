90b66bab22af54451070123a986c3a85eb2f03f3
sylvain tagnabou <sylvaintagnabou@gmail.com>
2023-01-31 20:40:22 +0000

    0x13 task05

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * free_listint2 - frees a listint_t list
+ * @head: pointer to the head of the list
+ *
+ * Return: void
+ */
+
+void free_listint2(listint_t **head)
+{
+listint_t *tmp;
+if (head == NULL)
+{
+return;
+}
+while (*head)
+{
+tmp = (*head)->next;
+free(*head);
+*head = tmp;
+}
+head = NULL;
+}
+
