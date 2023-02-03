432852dca2bcf73fd54a6eaac0af858b1615ba51
sylvain tagnabou <sylvaintagnabou@gmail.com>
2023-02-03 18:10:45 +0000

    0x14 task01

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_binary - prints the binary representation of a number
+ * @n: number to be printed
+ *
+ * Return: void
+ */
+
+void print_binary(unsigned long int n)
+{
+if (n > 1)
+{
+print_binary(n >> 1);
+}
+_putchar((n & 1) + '0');
+}
