cb2430bc23fd1202d45c4241f1f0826568e467c4
Sylvain T <sylvaintagnabou@gmail.com>
2023-03-16 22:20:28 +0000

    0x17 task03

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -10,7 +10,7 @@
 size_t dlistint_len(const dlistint_t *h)
 {
 size_t count = 0;
-while (h->next != NULL)
+while (h != NULL)
 {
 count++;
 h = h->next;
---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint_end - adds a new node at
+ * the end of a dlistint_t list
+ * @head: pointer to the head of the list
+ * @n: integer to be added to the list
+ * Return: address of the new element, or NULL if it failed
+ */
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+dlistint_t *new_node = (dlistint_t *)malloc(sizeof(dlistint_t));
+if (new_node == NULL)
+{
+return (NULL);
+}
+new_node->n = n;
+new_node->next = NULL;
+
+if (*head == NULL)
+{
+*new_node->prev = NULL;
+*head = new_node;
+return (new_node);
+}
+
+dlistint_t *current = *head;
+while (current->next != NULL)
+{
+current = current->next;
+}
+current->next = new_node;
+new_node->prev = current;
+return (new_node);
+}
