4b3d402ed829e3ee22b57766d2dd7a0dec99a870
Sylvain T <sylvaintagnabou@gmail.com>
2023-04-07 21:36:50 +0000

    0x1A task01__02

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -6,6 +6,7 @@
  *
  * Return: hash value
  */
+
 unsigned long int hash_djb2(const unsigned char *str)
 {
 unsigned long int hash;
@@ -14,7 +15,7 @@ int c;
 hash = 5381;
 while ((c = *str++))
 {
-hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+hash = ((hash << 5) + hash) +c; /* hash * 33 + c */
 }
 return (hash);
 }
