521a8089a4fab1e7f3bde6e70fb36ceb1ed2c74a
Sylvain T <sylvaintagnabou@gmail.com>
2023-04-07 21:37:48 +0000

    0x1a task02

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -7,7 +7,9 @@
  * Return: index at which the key/value pair should be stored in the array of the hash table
  */
 
-unsigned long int key_index(const unsigned char *key, unsigned long int size)
+unsigned long int key_index(
+const unsigned char *key, unsigned long int size
+)
 {
 return (hash_djb2(key) % size);
 }
