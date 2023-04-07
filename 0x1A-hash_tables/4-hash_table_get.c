bdf6764d147337d85418e43f9bc6b3d79f419f59
Sylvain T <sylvaintagnabou@gmail.com>
2023-04-07 22:20:11 +0000

    0x1A task04

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -10,5 +10,20 @@
 
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
+unsigned long int index = 0;
+hash_node_t *tmp = NULL;
 
+if (ht == NULL || key == NULL || strlen(key) == 0)
+return (NULL);
+
+index = key_index((const unsigned char *)key, ht->size);
+tmp = ht->array[index];
+if (tmp == NULL)
+return (NULL);
+
+while (strcmp(tmp->key, key) != 0)
+{
+tmp = tmp->next;
+}
+return (tmp->value);
 }
