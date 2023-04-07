9a5cd93e540c164b591791021342180ff68ab9de
Sylvain T <sylvaintagnabou@gmail.com>
2023-04-07 21:39:31 +0000

    0x1A task02

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -4,7 +4,8 @@
  * key_index - gives you the index of a key
  * @key: key
  * @size: size of the array of the hash table
- * Return: index at which the key/value pair should be stored in the array of the hash table
+ * Return: index at which the key/value
+ * pair should be stored in the array of the hash table
  */
 
 unsigned long int key_index(
