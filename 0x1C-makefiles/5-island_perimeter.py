daa5a1b2ac69e78ea61fd7ae00ec1a050a29e17c
Sylvain T <sylvaintagnabou@gmail.com>
2023-05-24 22:24:28 +0000

    ✅

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python3
+"""returns the perimeter of the island described in grid"""
+
 
 def island_perimeter(grid):
-    """returns the perimeter of the island described in grid"""
+    """args: grid: list of list of integers"""
     
-    e = 0
-    s = 0
+    width = len(grid[0])
+    height = len(grid)
+    edges = 0
+    size = 0
 
-    for i in range (len(grid)):
-        for j in range (len(grid[i])):
+    for i in range(height):
+        for j in range(width):
             if grid[i][j] == 1:
-                s += 1
-                if j > 0 and grid[i][j - 1] == 1:
-                    e += 1
-                if i > 0 and grid[i - 1][j] == 1:
-                    e += 1
-            
-    return s * 4 - e * 2
+                size += 1
+                if (j > 0 and grid[i][j - 1] == 1):
+                    edges += 1
+                if (i > 0 and grid[i - 1][j] == 1):
+                    edges += 1
+    return size * 4 - edges * 2
